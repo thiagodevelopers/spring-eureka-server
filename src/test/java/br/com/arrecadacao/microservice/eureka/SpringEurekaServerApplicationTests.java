1bcb7e7bc8a2170e293457914d8572ef3092f7c4
Thiago de Souza Santos <tssantos@tjrj.jus.br>
2021-04-30 18:40:36 -0300

    Create Eureka Server

---
+++ src/test/java/br/com/arrecadacao/microservice/eureka/SpringEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.arrecadacao.microservice.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
